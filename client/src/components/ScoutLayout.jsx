608f703c01dc21163c608aeae4f55d74ef293a86
jdlms <97018563+jdlms@users.noreply.github.com>
2022-12-15 21:29:57 +0100

    mui nav

--- client/src/components/ScoutLayout.jsx
+++ client/src/components/ScoutLayout.jsx
@@ -1,16 +1,46 @@
-import { Outlet } from "react-router-dom";
-import { ScoutNav } from "./ScoutNav";
+// import { Outlet } from "react-router-dom";
+// import { ScoutNav } from "./ScoutNav";
+
+// export function ScoutLayout() {
+//   return (
+//     <div>
+//       <ScoutNav />
+//       <main>
+//         <article>
+//           <Outlet />
+//         </article>
+//       </main>
+//       {/* <Footer /> */}
+//     </div>
+//   );
+// }
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+// import MenuIcon from "@mui/icons-material/Menu";
 
 export function ScoutLayout() {
   return (
-    <div>
-      <ScoutNav />
-      <main>
-        <article>
-          <Outlet />
-        </article>
-      </main>
-      {/* <Footer /> */}
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          ></IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
