ead216e7aa39acab6e74c85acb28857bc0f20a88
jdlms <lamorey.joshua@gmail.com>
2022-12-17 15:43:28 +0100

    signup validation

--- client/src/pages/login/Signup.jsx
+++ client/src/pages/login/Signup.jsx
@@ -3,16 +3,33 @@ import { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../contexts/UserContext";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export function Signup() {
   const { user, addUserToContext } = useContext(UserContext);
   const navigate = useNavigate();
 
-  const { register, handleSubmit } = useForm({
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required("Email is required").email("Email is invalid"),
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required"),
+    // confirmPassword: Yup.string()
+    //     .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    //     .required('Confirm Password is required'),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     defaultValues: {
       email: "",
       password: "",
     },
+    resolver: yupResolver(validationSchema),
   });
 
   const onSubmitSignup = async (data) => {
@@ -36,12 +53,10 @@ export function Signup() {
     <div>
       <h4>Signup</h4>
       <form key={1} onSubmit={handleSubmit(onSubmitSignup)}>
-        <input {...register("email", { required: true, minLength: 1 })} placeholder="email" />
-        <input
-          type="password"
-          {...register("password", { required: true, minLength: 1 })}
-          placeholder="password"
-        />
+        <input {...register("email")} placeholder="email" />
+        <div className="invalid-feedback">{errors.email?.message}</div>
+        <input type="password" {...register("password")} placeholder="password" />
+        <div className="invalid-feedback">{errors.password?.message}</div>
         <button type="submit">Send</button>
       </form>
     </div>
