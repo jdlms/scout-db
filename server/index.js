c617b62c8349f73176b285ce3e73d6f371857505
jdlms <97018563+jdlms@users.noreply.github.com>
2023-01-12 21:22:34 +0100

    cors update

--- server/index.js
+++ server/index.js
@@ -41,12 +41,6 @@ app.use(
   })
 );
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
