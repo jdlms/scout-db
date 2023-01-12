44162054013a1733e54fc9d205f2868f45444b88
jdlms <97018563+jdlms@users.noreply.github.com>
2023-01-12 21:12:39 +0100

    cors header update

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ app.use(
 );
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
