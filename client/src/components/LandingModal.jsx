ba807b819627c33a2856fd162b608b11a71d17e2
jdlms <97018563+jdlms@users.noreply.github.com>
2023-02-19 11:14:44 +0100

    modal text edit

--- client/src/components/LandingModal.jsx
+++ client/src/components/LandingModal.jsx
@@ -37,17 +37,44 @@ export function LandingModal({ open, handleOpen, handleClose }) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
+          <Typography id="modal-modal-title" variant="h6">
             What is this?
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2}}>
-            Scout DB is a... Duis mollis, est non commodo luctus, nisi erat porttitor ligula. Duis mollis, est non cs, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          <Typography id="modal-modal-description" variant="body2" gutterBottom sx={{ mt: 2 }}>
+            Scout DB is a prototype of an internal database and reporting tool built for book scouts
+            and their clients.
+          </Typography>
+          <Typography id="modal-modal-title" variant="h6" component="h3">
+            What is the problem?
+          </Typography>
+          <Typography id="modal-modal-description" variant="body2" gutterBottom sx={{ mt: 2 }}>
+            Scouts gather large amounts of information and report it to clients. Often this
+            reporting is done via private local databases, email, phone, and pdfs. This is
+            inefficient and communication can be haphazard or sporadic.
+          </Typography>
+          <Typography id="modal-modal-title" variant="h6" component="h3">
+            What is the solution?
+          </Typography>
+          <Typography id="modal-modal-description" variant="body2" gutterBottom sx={{ mt: 2 }}>
+            Scout DB offers a streamlined way for book scouts to interact with their database, input
+            information, track titles, and report to clients. For clients, it provides an online
+            portal where large swaths of information can be easily managed, referenced, and
+            highlighted.
+          </Typography>
+          <Typography id="modal-modal-title" variant="h6" component="h3">
+            Ok, but what is a book scout?
+          </Typography>
+          <Typography id="modal-modal-description" variant="body2" gutterBottom sx={{ mt: 2 }}>
+            A book scout helps international publishers, literary agents, and film production
+            companies find new and worthwhile books to acquire for publication or adaptation. They
+            may work independently or for a scouting agency and are often experts in a specific
+            genre or market. They read widely, stay up-to-date on industry trends, and have a keen
+            eye for identifying potential bestsellers or overlooked gems. They often have extensive
+            contacts within the publishing industry and can help match books with the right
+            publisher or production company.
           </Typography>
         </Box>
       </Modal>
     </div>
   );
 }
-
-
-//A book scout is a professional who helps publishers, literary agents, and film production companies find new and worthwhile books to acquire for publication or adaptation. They may work independently or for a scouting agency and are often experts in a specific genre or market. They read widely, stay up-to-date on industry trends, and have a keen eye for identifying potential bestsellers or literary gems. They often have extensive contacts within the publishing industry and can help match books with the right publisher or production company.
